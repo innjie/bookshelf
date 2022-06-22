abee36a042c08c4d585dd72165e4ff11fdc5f620
innjie <innjie@naver.com>
2022-06-22 15:01:32 +0900

    [update] position contents

--- front/src/statics/Nav.js
+++ front/src/statics/Nav.js
@@ -7,34 +7,31 @@ import {
     Route,
     Link
 } from 'react-router-dom';
+
 function Nav() {
     return (
-        <Container fluid>
-            <aside className="App-nav">
-                <div className="Nav-Content row">
-                    {/*로그인상태*/}
-                    <div className="id-section">
-                        <ul>
-                            aaa
-                        </ul>
+        <aside className="App-nav">
+            {/*로그인상태*/}
+            <div className="id-section">
+                <ul>
+                    aaa
+                </ul>
 
-                        <ul className="content-section ">
-                            <a href="/account/update" className="col-xs-4">정보 수정</a>
-                            &nbsp;&nbsp;&nbsp;
-                            <a href="/account/bookmark" className="col-xs-4">북마크</a>
-                        </ul>
-                    </div>
-                    <div className="menu-section">
-                        {/*리스트*/}
-                        <ul><a href="/view/all">전체 보기</a></ul>
-                        <ul><Link to="/view/profile">프로필별 보기</Link></ul>
-                    </div>
-                    <div className="banner-section">
-                        {/*배너(디스코드)*/}
-                    </div>
-                </div>
-            </aside>
-        </Container>
+                <ul className="content-section ">
+                    <a href="/account/update" className="col-xs-4">정보 수정</a>
+                    &nbsp;&nbsp;&nbsp;
+                    <a href="/account/bookmark" className="col-xs-4">북마크</a>
+                </ul>
+            </div>
+            <div className="menu-section">
+                {/*리스트*/}
+                <ul><a href="/view/all">전체 보기</a></ul>
+                <ul><Link to="/view/profile">프로필별 보기</Link></ul>
+            </div>
+            <div className="banner-section">
+                {/*배너(디스코드)*/}
+            </div>
+        </aside>
     );
 }
 
