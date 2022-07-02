32a67ff76deede0e40ac84e693a2538f3b0c1002
innjie <innjie@naver.com>
2022-07-03 00:47:24 +0900

    [update] notice detail react

--- front/src/pages/notice/NoticeMain.js
+++ front/src/pages/notice/NoticeMain.js
@@ -1,30 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './NoticeMain.css';
+import NoticeDetail from "./NoticeDetail";
 
 function NoticeMain() {
-
+    const [isList, setListState] = useState(true);
     const [noticeList, setList] = useState([{
         idx: '',
         title: '',
         content: '',
         updateDate : ''
     }]);
-    // let array = list.map((value, key) => <li key = {key}>value</li>);
     useEffect(() => {
         axios.get("/notice/list")
             .then(res => setList(res.data.noticeList))
             .catch(error => console.log(error))
 
     }, []);
-
-
     function noticeDetail(idx) {
-        const result = axios.get("/notice/detail/", {
-            params : {
-                idx : idx
-            }
-        });
+        setListState(false);
+
     }
     return (
         <div>
@@ -48,7 +43,13 @@ function NoticeMain() {
                         return (
                             <tr key={notice.idx}>
                                 <td>{notice.idx}</td>
-                                <td onClick={noticeDetail(notice.idx)}>{notice.title}</td>
+                                <td onClick={function (e) {
+                                    e.preventDefault();
+                                    setListState(true);
+                                    if(isList) {
+                                        return <NoticeDetail idx={notice.idx}/>
+                                    }
+                                }.bind(this)}>{notice.title}</td>
                                 <td>{notice.updateDate.split("T")[0]}</td>
                             </tr>
                         )
