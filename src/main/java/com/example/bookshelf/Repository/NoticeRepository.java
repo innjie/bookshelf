773d6ed115817f7ae40c146233577d900245a6bb
innjie <innjie@naver.com>
2022-07-08 18:15:42 +0900

    [update] notice insert func

--- src/main/java/com/example/bookshelf/Repository/NoticeRepository.java
+++ src/main/java/com/example/bookshelf/Repository/NoticeRepository.java
@@ -2,7 +2,9 @@ package com.example.bookshelf.Repository;
 
 import com.example.bookshelf.Domain.NoticeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,6 +13,8 @@ public interface NoticeRepository extends JpaRepository<NoticeDTO, Integer> {
     List<NoticeDTO> getNoticeList();
     @Query(value = "SELECT * FROM NoticeTbl WHERE idx = :idx", nativeQuery = true)
     NoticeDTO getNotice(long idx);
-    @Query(value = "INSERT INTO NoticeTbl (title, contents) VALUES(#{#notice.title}, #{#notice.contents})", nativeQuery = true)
-    void insertNotice(NoticeDTO notice);
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO NoticeTbl (title, contents) VALUES(:#{#notice.title}, :#{#notice.contents})", nativeQuery = true)
+    int insertNotice(NoticeDTO notice);
 }
