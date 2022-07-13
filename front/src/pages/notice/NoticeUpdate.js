d5ac7c31f694cb90303a34394c8a4708899d9384
innjie <innjie@naver.com>
2022-07-13 23:34:28 +0900

    [update] notice udpate

--- front/src/pages/notice/NoticeUpdate.js
+++ front/src/pages/notice/NoticeUpdate.js
@@ -19,25 +19,21 @@ function NoticeUpdate() {
         updateDate: ''
     });
     const [title, setTitle] = useState('');
-    const handleTitle = useCallback(e => {
-        setTitle(e.target.value);
-    }, []);
+
     const [contents, setContents] = useState('');
-    const handleContents = useCallback(e => {
-        setContents(e.target.value);
-    }, []);
+
     // update notice
     const updateNotice = (idx) => {
         axios.put("/notice/update", null, {
             params: {
-                idx : idx,
-                title :title,
-                contents : contents
+                idx: idx,
+                title: title,
+                contents: contents
             }
         }).then(res => alert(res.data.result))
             .catch(error => console.log(error));
 
-        window.location.href="/notice/list";
+        window.location.href = "/notice/list";
     }
     //get object
     useEffect(() => {
@@ -47,31 +43,41 @@ function NoticeUpdate() {
             }
         })
             .then(res => {
+
                 setNotice(res.data.notice);
+                console.log(res.data.notice);
                 console.log(notice);
                 setTitle(notice.title);
-                console.log(title);
+                console.log("title : " + title);
                 setContents(notice.contents);
-                console.log(contents);
+                console.log("contents : "  + contents);
             })
             .catch(error => console.log(error));
+    },[]);
+    const handleTitle = useCallback(e => {
+        setTitle(e.target.value);
     }, []);
-
+    const handleContents = useCallback(e => {
+        setContents(e.target.value);
+    }, []);
     return (
         <div className="notice-form">
             {/*제목*/}
             <div className="insert-title-section">
-                <input type="text" value = {title} className="insert-title" onChange={handleTitle}/>
+                <input type="text" value={title} className="insert-title" onChange={handleTitle}/>
             </div>
 
             {/*내용*/}
             <div className="insert-contents-section">
                 <hr/>
-                <textarea value = {contents} className="insert-contents" onChange={handleContents} placeholder="내용을 입력하세요">
+                <textarea value={contents} className="insert-contents" onChange={handleContents}
+                          placeholder="내용을 입력하세요">
                 </textarea>
             </div>
-            <input type="button" value="수정하기" onClick={() => updateNotice(keyword)} className="thumbsUp btn btn-success btn-sm"/>
-            <input type="button" value="뒤로가기" onClick={() => handleHistory} className="backToPage btn btn-toolbar btn-sm"/>
+            <input type="button" value="수정하기" onClick={() => updateNotice(keyword)}
+                   className="thumbsUp btn btn-success btn-sm"/>
+            <input type="button" value="뒤로가기" onClick={() => handleHistory}
+                   className="backToPage btn btn-toolbar btn-sm"/>
 
         </div>
     );
