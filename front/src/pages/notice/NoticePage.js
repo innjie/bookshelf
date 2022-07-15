0de342674794bb775fd86ab0534ab0920b62a92c
innjie <innjie@naver.com>
2022-07-15 21:09:10 +0900

    [update] notice udpate

---
+++ front/src/pages/notice/NoticePage.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import './NoticeDetail.css';
+import {getIdx} from '../../js/functions';
+import {useLocation} from "react-router-dom";
+import {useHistory} from 'react-router-dom';
+import {Link} from "react-router-dom";
+
+function NoticePage() {
+    const location = useLocation();
+    const keyword = getIdx(location);
+    const history = useHistory();
+
+    const handleHistory = () => {
+        history.goBack();
+    }
+    const [state, setState] = useState('detail');
+    const [notice, setNotice] = useState({
+        idx: '',
+        title: '',
+        contents: '',
+        updateDate: ''
+    });
+    useEffect(() => {
+        axios.get("/notice/detail", {
+            params: {
+                idx: parseInt(keyword)
+            }
+        })
+            .then(res => setNotice(res.data.notice))
+            .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <div className="noticeContent">
+            <if state === 'detail'>
+
+            </if>
+            <div className="update">
+
+            </div>
+
+
+        </div>
+    );
+}
+
+function getDetailContent() {
+    return (
+        <div className="detail">
+            <div>
+                {notice.title} <br/>
+                {/*조회수*/}
+            </div>
+            <div>
+                <hr/>
+                <textarea readOnly={true} value={notice.contents}>
+                </textarea>
+
+            </div>
+            <input type="button" value="좋아요" className="thumbsUp btn btn-success btn-sm"/>
+
+            <input type="button" value="수정하기"
+                   onClick={() => updateNotice(notice.idx)}
+                   className="btn btn-sm"/>
+            <input type="button" value="뒤로가기" onClick={handleHistory}
+                   className="backToPage btn btn-toolbar btn-sm"/>
+        </div>
+    )
+}
+
+function getUpdateContent() {
+
+}
+
+export default NoticePage;
