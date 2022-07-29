a4f125dac3ccc9de51cb0c44ea723ad99509cc4b
이인지 <innjie@naver.com>
2022-07-29 09:19:18 +0900

    [update] profile insert form

--- front/src/pages/profile/ProfileInsert.js
+++ front/src/pages/profile/ProfileInsert.js
@@ -3,26 +3,61 @@ import axios from 'axios';
 import './ProfileInsert.css'
 
 function ProfileInsert() {
+    const [nickname, setNickname] = useState('');
+    const handleNickname = (e) =>  {
+        setNickname(e.target.value);
+    }
+    const [id, setId] = useState('');
+    const handleId = (e) => {
+        setId(e.target.value);
+    }
+    const [password, setPassword] = useState('');
+    const handlePassword = (e) =>  {
+        setPassword(e.target.value);
+    }
+    const [profileImg, setProfileImg] = useState(null);
+    const handleProfileImg = (e) =>  {
+        setProfileImg(e.target.value);
+    }
+    const insertProfile = () => {
+        console.log("nickname : " + nickname);
+        console.log("id : " + id);
+        console.log("password: " + password);
+        console.log("profileImg : " + profileImg);
+
+        // axios.post("/profile/insert", null, {
+        //     params : {
+        //         nickname : nickname,
+        //         id : id,
+        //         password : password,
+        //         profileImg : profileImg
+        //     }
+        // }).then(res => alert(res.data.result))
+        //     .catch(error => console.log(error));
+
+        // window.location.href = "/prifle/list";
+    }
     return (
         <div className="insert-form">
             <form>
                 {/*nickname*/}
                 <p>nickname</p>
-                <input type="text" className="insert-contents insert-nickname"/> <br/>
+                <input type="text" onChange={handleNickname} className="insert-contents insert-nickname"/> <br/>
                 {/*id*/}
                 <p>id</p>
-                <input type="text" className="insert-contents insert-id"/> <br/>
+                <input type="text" onChange={handleId} className="insert-contents insert-id"/> <br/>
                 {/*password*/}
                 <p>password</p>
-                <input type="password" className="insert-contents insert-password"/> <br/>
+                <input type="password" onChange={handlePassword} className="insert-contents insert-password"/> <br/>
                 {/*profileImg*/}
                 <p>image</p>
-                <input type="file" className="insert-contents insert-img"/> <br/>
+                <input type="file" onChange={handleProfileImg} multiple="multiple" className="insert-contents insert-img"/> <br/>
             </form>
 
             <div className="btn-insert">
                 {/*send*/}
-                <input type="button" className="btn-insert" value="등록하기"/> <br/>
+                <input type="button" className="btn-insert" value="등록하기"
+                onClick = {insertProfile} /> <br/>
             </div>
         </div>
     )
