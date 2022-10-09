9682081c406305895a3c6a639b834a1f1011f5fa
innjie <innjie@naver.com>
2022-10-10 00:09:03 +0900

    [update] PROFILE list

--- front/src/pages/profile/Profile.js
+++ front/src/pages/profile/Profile.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {
+    Link
+} from "react-router-dom";
 
 function Profile() {
     const [profileList, setList] = useState([{
